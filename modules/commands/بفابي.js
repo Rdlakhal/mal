6e14ce759775f857cdef453aaa80924e9f9ead22
shamkhacha <160078586+shamkhacha@users.noreply.github.com>
2024-04-19 21:01:27 +0200

    Create بفابي.js

---
+++ modules/commands/بفابي.js
@@ -0,0 +1,27 @@
+module.exports.config = {
+   name: "بنتي",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "3bida",
+    description: "",
+    usages: "",
+    commandCategory: "المطور",
+    cooldowns: 0,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["••>💀\n\n⎒❲ نعم بابي ❤😋😚❳⎒"];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = ["h"
+];
+	 var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
+   };
