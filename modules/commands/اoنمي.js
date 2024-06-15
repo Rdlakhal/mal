176d3d57dd844ebed207364d9a66c8f9bcc8a1ae
shamkhacha <160078586+shamkhacha@users.noreply.github.com>
2024-06-15 23:02:55 +0200

    Update اoنمي.js

--- modules/commands/اoنمي.js
+++ modules/commands/اoنمي.js
@@ -102,6 +102,6 @@ const fs = global.nodemodule["fs-extra"];
 "https://i.imgur.com/czusqtb.jpg",
 "https://i.imgur.com/WRCWzit.jpg"
      ];
-     var callback = () => api.sendMessage({body:`صور أنمي  `,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:` صور انمي من اجلك |✅`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
