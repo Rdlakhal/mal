513603ecf421eb1e137b9a234e57c43d21bd73b9
shamkhacha <160078586+shamkhacha@users.noreply.github.com>
2024-07-04 10:21:08 +0200

    Update سجننن.js

--- modules/commands/سجننن.js
+++ modules/commands/سجننن.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-	name: "سجن",
+	name: "بروفايل",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "عمر",
-	description: " حط صورتك خلف القضبان او الي تمنشنه او الي ترد عليه ",
-	commandCategory: "صور",
-	usages: " ",
+	credits: "Black Amex",
+	description: "بروفايلك او بروفايل الي تمنشنه او ترد عليه",
+	commandCategory: "تـــرفـیـه",
+	usages: "ا",
 	cooldowns: 5,
 	dependencies: {
 	  "fs-extra": "",
@@ -44,7 +44,7 @@ module.exports.run = async function ({ api, event, args, Users }) {
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  ctx.drawImage(baseAva, 0, 0, 1024, 2024);
+  ctx.drawImage(baseAva, 99999, 0, 1, 2);
   ctx.beginPath();
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
