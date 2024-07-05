59d163bd9f2a1a67f9135afc113d927fbf1463ae
shamkhacha <160078586+shamkhacha@users.noreply.github.com>
2024-07-05 21:37:31 +0200

    Create مملاك.js

---
+++ modules/commands/ملاك.js
@@ -0,0 +1,20 @@
+module.exports.config = {
+  name: "ملاك",
+  version: "1.0.0",
+  hasPermision: 2,
+  credits: "عمر",
+  description: "يرسل لك نكتة عشوائية",
+  usage: "نكته",
+  commandCategory: "ترفية",
+  cooldowns: 0,
+};
+
+module.exports.run = async function ({ api, event }) {
+  const jokes = [
+    "نعم باتي",
+    "عيونها",
+  ];
+
+  const joke = jokes[Math.floor(Math.random() * jokes.length)];
+  api.sendMessage(joke, event.threadID, event.messageID);
+};
