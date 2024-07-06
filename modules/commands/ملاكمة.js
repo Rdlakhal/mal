99513ec4bc6cfc583fd1aacc24753fef8ff2891a
shamkhacha <160078586+shamkhacha@users.noreply.github.com>
2024-07-06 10:48:50 +0200

    Update ملاكمة.js

--- modules/commands/ملاكمة.js
+++ modules/commands/ملاكمة.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "يوتا",
     version: "1.0.0",
-    hasPermssion: 2,
+    hasPermssion: 2, // تأكد من أن إذن المستخدم هو 2 (مسؤول)
     credits: "3bida",
     description: "",
     usages: "",
@@ -10,7 +10,13 @@ module.exports.config = {
 };
 
 module.exports.run = async({api, event, args, client, Users, Threads, __GLOBAL, Currencies}) => {
-    var hi = ["نعم بابي 😚🫣","عيونها 😚😊","نعم البي 🫢😊"];
+    // التحقق مما إذا كان المستخدم هو مسؤول في المجموعة
+    var info = await api.getThreadInfo(event.threadID);
+    if (!info.adminIDs.some(item => item.id == event.senderID)) {
+        return api.sendMessage('هذا الأمر مخصص للمسؤولين فقط.', event.threadID, event.messageID);
+    }
+
+    var hi = ["نعم بابي 😚🫣", "عيونها 😚😊", "نعم البي 🫢😊"];
     var know = hi[Math.floor(Math.random() * hi.length)];
 
     api.sendMessage(`${know}`, event.threadID);
