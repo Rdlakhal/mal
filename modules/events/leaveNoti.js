b03d78088ce41f868a0e9f399705a69f0b8a49fa
shamkhacha <160078586+shamkhacha@users.noreply.github.com>
2024-07-11 08:14:26 +0200

    Update leaveNoti.js

--- modules/events/leaveNoti.js
+++ modules/events/leaveNoti.js
@@ -1,43 +1,62 @@
-const axios = require("axios");
-
 module.exports.config = {
-	name: "leave",
-	eventType: ["log:unsubscribe"],
-	version: "1.0.0",
-	credits: "Mirai Team",
-	description: "Notify bots or leavers",
-	dependencies: {
-		"fs-extra": "",
-		"path": ""
-	}
+  name: "leave",
+  eventType: ["log:unsubscribe"],
+  version: "1.0.0",
+  credits: "عمر",//Mod by H.Thanh
+  description: "Notify the Bot or the person leaving the group with a random gif/photo/video",
+  dependencies: {
+    "fs-extra": "",
+    "path": ""
+  }
 };
 
+module.exports.onLoad = function () {
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { join } = global.nodemodule["path"];
+
+  const path = join(__dirname, "cache", "leaveGif");
+  if (existsSync(path)) mkdirSync(path, { recursive: true });	
+
+  const path2 = join(__dirname, "cache", "leaveGif", "randomgif");
+    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
+
+    return;
+}
+
 module.exports.run = async function({ api, event, Users, Threads }) {
-	if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-	const { join } =  global.nodemodule["path"];
-	const { threadID } = event;
 
+     if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+  const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+  const { join } =  global.nodemodule["path"];
+  const { threadID } = event;
+  const moment = require("moment-timezone");
+  const time = moment.tz("africa/morocco").format("DD/MM/YYYY || HH:mm:s");
+  const hours = moment.tz("africa/morocco").format("HH");
+  const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "ّ" : "ُ";
+  const path = join(__dirname, "cache", "leaveGif");
+  const pathGif = join(path, `${threadID}.gif`);
+  var msg, formPush
+  if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-const gifes = await axios.get(`https://i.ibb.co/rZZwCDC/289048626-3279695619019315-6860934043982649302-n-jpg-nc-cat-111-ccb-1-7-nc-sid-5f2048-nc-eui2-Ae-HN9.jpg`, { responseType: "stream"});
-		const atth = gifes.data;
+(typeof data.customLeave == "undefined") ? msg = "(ˉ﹃ˉ)" : msg = data.customLeave;
+  msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type).replace(/\{session}/g, hours <= 10 ? "صباح الخير للاعضاء المتبقين" : 
+    hours > 10 && hours <= 12 ? "مساء الخير للاعضاء المتبقين" :
+    hours > 12 && hours <= 18 ? "ليلة سعيدة للاعضاء المتبقين" : "ليلة سعيدة للاعضاء المتبقين").replace(/\{time}/g, time);  
+  if (event.logMessageData.leftParticipantFbId == '100094409873389')
+  {
+    msg = 'ايو هل حشرات كيف تتجرئون على طرد مطوري'
 
-	
-	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
-	
-	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "♧=「 غادر 」=♧" : "♧=「 اترمه 」=♧";
-	const path = join(__dirname, "cache", "leachfvveGif");
-	const gifPath = join(path, `byxcve2.jpg`);
-	var msg, formPush
+  }
+  const randomPath = readdirSync(join(__dirname, "cache", "leaveGif", "randomgif"));
 
-	if (existsSync(path)) mkdirSync(path, { recursive: true });
+  if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif) }
+  else if (randomPath.length != 0) {
+    const pathRandom = join(__dirname, "cache", "leaveGif", "randomgif",`${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
+    formPush = { body: msg, attachment: createReadStream(pathRandom) }
+  }
+  else formPush = { body: msg }
 
-	(typeof data.customLeave == "undefined") ? msg = "💔    「{name}」   💔 \n「♕」لا تـحـزن عـلـيـهـا يا قـلـبـي فـانـهـا تهوا الـفـراق فـرحـلـت「♕」\n💔       {type}     💔" : msg = data.customLeave;
-	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
-
-	if (existsSync(gifPath)) formPush = { body: msg, attachment: atth }
-	else formPush = { body: msg, attachment: atth }
-	
-	return api.sendMessage(formPush, threadID);
+  return api.sendMessage(formPush, threadID);
 }
