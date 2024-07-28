b367522815ee31f2ccb203fbb7cd96c0d1fc9bb7
shamkhacha <160078586+shamkhacha@users.noreply.github.com>
2024-07-28 10:50:36 +0200

    Update ذكاأ.js

--- modules/commands/ذكاأ.js
+++ modules/commands/ذكاأ.js
@@ -24,7 +24,7 @@ const aa = {
   run: async function({ event, api, args}) {
   
     const coj = args.join(" ")
-    if (!coj) return out('💙','🔥')
+    if (!coj) return out('💙')
 async function out(gry, callback)  {
 await api.sendMessage(gry, event.threadID, callback, event.messageID);
 };
